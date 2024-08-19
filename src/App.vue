9323379a185ed5beb7a399e37b67781acca15df9
Ehya-Ag <ehyaagmohamed23@gmail.com>
2024-08-19 22:14:03 +0000

    Ajouter le composants Livre(ajout, suppression et modification)

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
 import { RouterLink, RouterView } from 'vue-router'
 
 import HelloWorld from './components/HelloWorld.vue'
+<<<<<<< HEAD
+import listerMembres from './components/Membres/listerMembres.vue';
+import addMemberjouterMembre from './components/Membres/ajouterMembre.vue';
+import modifierMembre from './components/Membres/modifierMembre.vue';
+import navbar from './components/navbar.vue'
 </script>
 
 <template>
   <h1>Gestion bibliothéque</h1>
+  <listerMembres/>
+  <AjouterMembre/>
+  <modifierMembre/>
   <HelloWorld/>
   <RouterView />
-  </div>
-
- 
 </template>
 
 <style scoped>
