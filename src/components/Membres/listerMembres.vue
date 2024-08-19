4aa3b71b39f6f943012b97acfdaf1e12eb04f820
ousmane <ousmanely11@gmail.com>
2024-08-19 23:40:47 +0000

    Ajouter composition membres(suppression, modification et ajout)

--- src/components/Membres/listerMembres.vue
+++ src/components/Membres/listerMembres.vue
@@ -104,8 +104,8 @@
   import { ref } from 'vue';
   
   const members = ref([
-    { id: 1, nom: 'Ehya Agg', email: 'agg@example.com', adresse: '10 Rue de Paris' },
-    { id: 2, nom: 'Ly Ousmane', email: 'ousmane@example.com', adresse: '20 Rue Basra' }
+    { id: 1, nom: 'Ehya Agg', email: 'ag@example.com', adresse: '10 Rue de Paris' },
+    { id: 2, nom: 'Marie Curie', email: 'marie.curie@example.com', adresse: '20 Rue des Sciences' }
   ]);
   
   const showAddMemberForm = ref(false);
