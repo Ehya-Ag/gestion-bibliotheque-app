fe8b95ab88558b4c8a921eec68a4d1ae4d63bc5d
Ehya-Ag <ehyaagmohamed23@gmail.com>
2024-08-19 12:00:47 +0000

    ajout de bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // import './assets/main.css'
-
+import "bootstrap/dist/js/bootstrap.js"
+import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
